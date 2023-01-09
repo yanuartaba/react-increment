d9a09f9644e3996fbdc43ecf88bf9775e263816a
Aray Yanuartaba <arieyanuartaba@yahoo.co.id>
2023-01-09 08:51:11 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [number, setNumber] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="flex justify-center items-center min-h-screen">
+        <div className="flex flex-col gap-3">
+          <h1 class="text-2xl font-bold underline">Latihan Increment</h1>
+          <h1 className="text-5xl text-purple-600 font-bold font-mono text-center">
+            {number}
+          </h1>
+          <div className="flex justify-center gap-3">
+            <button
+              className="p-3 bg-green-500 text-gray-200 font-bold rounded"
+              onClick={() => {
+                let addOne = number + 1;
+                setNumber(addOne);
+              }}
+            >
+              Add 1 (+)
+            </button>
+            <button
+              className="p-3 bg-gray-500 text-gray-200 font-bold rounded"
+              onClick={() => {
+                let minOne = number - 1;
+                if (number !== 0) {
+                  setNumber(minOne);
+                }
+              }}
+            >
+              Min 1 (-)
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
